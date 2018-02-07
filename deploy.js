4cbb027d8605ce936a4ff613ca27715fd915cad2
HyphnKnight <mynameisrafe@google.com>
2018-02-07 01:05:03 -0500

    improve deployment messaging and recovery

--- deploy.js
+++ deploy.js
@@ -20,73 +20,93 @@ const writeFile = util.promisify(wf);
  * Checkout dev branch
  */
 
+const logError =
+  (errorText) =>
+    console.log(`\x1b[31m%s\x1b[0m`, errorText);
+
+const logFocus =
+  (text) =>
+  console.log(`\x1b[32m%s\x1b[0m`, text);
+
 async function deploy() {
-  console.log(`Deploying Pura.`);
+  logFocus(`Deploying Pura.`);
   try {
+    console.log(`Checking for uncommited files.`);
     await exec(`git status | grep "working tree clean"`);
   } catch (e) {
-    console.log(`Deploy failed, can not deploy with uncommited code.`);
+    logError(`Deploy failed, can not deploy with uncommited code.`);
     process.exit(0);
   }
   try {
-    await exec(`git branch | grep "*"`);
+    console.log('Checking branch name.');
+    await exec(`git branch | grep "* dev"`);
   } catch (e) {
-    console.log(`Deploy failed, can only deploy when on the dev branch.`);
+    logError(`Deploy failed, can only deploy when on the dev branch.`);
     process.exit(0);
   }
-  console.log(`Compiling Pura.`)
   try {
+    console.log(`Compiling Pura.`)
     await exec(`npm run compile`);
   } catch (e) {
-    console.log(`Deploy failed, can not deploy the build failed.`);
+    logError(`Deploy failed, can not deploy the build failed.`);
     console.log(e);
-    // process.exit(0);
+    process.exit(0);
   }
   let version;
   try {
+    console.log(`Checking package version.`);
     const packageStr = await readFile(`./package.json`, `utf8`);
     version = JSON.parse(packageStr).version;
   } catch (e) {
-    console.log(`Deploy failed, unable to read the package.json`);
+    logError(`Deploy failed, unable to read the package.json.`);
     console.log(e);
     process.exit(0);
   }
   try {
+    console.log(`Switching to master branch.`);
     await exec(`git checkout master`);
   } catch (e) {
-    console.log(`Deploy failed, unable to switch to the master branch.`);
+    logError(`Deploy failed, unable to switch to the master branch.`);
     console.log(e);
     process.exit(0);
   }
   try {
+    console.log(`Checking and updating package version.`);
     const packageStr = await readFile(`./package.json`, `utf8`);
     const package = JSON.parse(packageStr);
     if (version === package.version) {
-      console.log(`Deploy failed, new version is the same as the old version.`);
-      console.log(e);
+      await exec(`git reset HEAD --hard`);
+      await exec(`git checkout dev`);
+      logError(`Deploy failed, new version is the same as the old version.`);
       process.exit(0);
     }
     package.version = version;
     await writeFile('./package.json', JSON.stringify(package));
   } catch (e) {
-    console.log(`Deploy failed, unable to read/write package.json on the master branch.`);
+    await exec(`git reset HEAD --hard`);
+    await exec(`git checkout dev`);
+    logError(`Deploy failed, unable to read/write package.json on the master branch.`);
     console.log(e);
     process.exit(0);
   }
   try {
-    await exec(`mv ./dist/** ./`);
+    console.log(`Moving newer files out of dist folder.`);
+    await exec(`mv -u ./dist/** ./`);
     await exec(`git add .`);
+    console.log(`Commiting new version of the project.`);
     await exec(`git commit -m 'version@${version}'`);
+    console.log(`Push new version of master.`);
     await exec(`git push origin master`);
+    console.log(`Return to the dev branch.`);
     await exec(`git checkout dev`);
   } catch (e) {
-    console.log(`Deploy failed, unable to move files during the build.`);
+    logError(`Deploy failed, unable to finish the commit of new files.`);
     await exec(`git reset HEAD --hard`);
     await exec(`git checkout dev`);
     console.log(e);
     process.exit(0);
   }
-  console.log(`Deploy Succeeded, version ${version} is now live.`);
+  logFocus(`Deploy Succeeded, version ${version} is now live.`);
 }
 
 deploy();
