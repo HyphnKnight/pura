79943473a8bb9862ba8bf93e43aaf5a1097be621
HyphnKnight <rafe.architect@gmail.com>
2020-01-31 23:45:58 -0500

    Add additional types.

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,3 +1,20 @@
-export type Diff<T extends string, U extends string> = ({[P in T]: P } & {[P in U]: never } & { [x: string]: never })[T];
+export type KeyType = string | number | symbol;
 
-export type Omit<T, K extends keyof T> = {[P in Diff<keyof T, K>]: T[P]};
+export type Diff<T extends KeyType, U extends KeyType> = ({ [P in T]: P } & { [P in U]: never } & { [x: string]: never })[T];
+
+export type Omit<T, K extends keyof T> = { [P in Diff<keyof T, K>]: T[P] };
+
+export type Matrix<Value = number> = Value[][];
+
+export type Matrix4<Value = number> = [
+  [Value, Value, Value, Value],
+  [Value, Value, Value, Value],
+  [Value, Value, Value, Value],
+  [Value, Value, Value, Value],
+];
+
+export type Matrix3<Value = number> = [
+  [Value, Value, Value],
+  [Value, Value, Value],
+  [Value, Value, Value],
+];
